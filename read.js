672b8e2fd5d82ed3301cef53e120e4cb13f465ac
krishna chavali <kclogics@gmail.com>
2017-11-21 09:30:14 +0530

    Update read.js

--- read.js
+++ read.js
@@ -1,12 +1,16 @@
- var start = '{"name":"krishna Chavali" , "position":"Full Stack Web Dev" }';
+//Javascript Document
+
+console.log("from js/main.js");
+
+var start = '{"name":"krishna Chavali" , "position":"Full Stack Web Dev" }';
 
 
 
 var myObject = JSON.parse(start);
 
 console.log(myObject);
-console.log(myObject.name);
-console.log(myObject.position);
+/*console.log(myObject.name);
+console.log(myObject.position);*/
 
 document.getElementById("name").innerHTML = myObject.name;
 document.getElementById("position").innerHTML = myObject.position;
@@ -16,4 +20,5 @@ var myString = JSON.stringify(myObject);
 console.log(myString);
 
 
-
+var myObject = JSON.parse(myString);
+console.log(myObject)
