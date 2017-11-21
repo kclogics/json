ed00c2b37c7a3ceb9293dd32c89f89d8aac11de0
krishna chavali <kclogics@gmail.com>
2017-11-21 09:23:26 +0530

    Update and rename README.md to read.js

---
+++ read.js
@@ -0,0 +1,19 @@
+ var start = '{"name":"krishna Chavali" , "position":"Full Stack Web Dev" }';
+
+
+
+var myObject = JSON.parse(start);
+
+console.log(myObject);
+console.log(myObject.name);
+console.log(myObject.position);
+
+document.getElementById("name").innerHTML = myObject.name;
+document.getElementById("position").innerHTML = myObject.position;
+
+
+var myString = JSON.stringify(myObject);
+console.log(myString);
+
+
+
